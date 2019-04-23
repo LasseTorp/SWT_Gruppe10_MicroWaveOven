8c2dff27c6adddeff93ebabdc7078ed8120b7ad9
JeppeChr <jeppechristensen2@gmail.com>
2019-04-23 16:17:16 +0200

    IT8 klargøring

---
+++ Microwave.test.integration/IT8_DoorUserInterface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Microwave.test.integration
+{
+
+    [TestFixture]
+    class IT8_DoorUserInterface
+    {
+        private Door uutDoor_;
+        private ILight light_;
+        private IDisplay display_;
+        private IPowerTube powerTube_;
+        private ITimer timer_;
+        private ICookController cookController_;
+        private IUserInterface userInterface_;
+
+        private IOutput output_;
+
+        //knapperne skal oprettes for at kunne oprette userinterface rigtigt længere nede???
+
+        [SetUp]
+        public void setup()
+        {
+            timer_ = Substitute.For<ITimer>();
+
+            output_ = new Output();
+
+            light_ = new Light(output_);
+            display_ = new Display(output_);
+            powerTube_ = new PowerTube(output_);
+
+            cookController_ = new CookController(timer_,display_,powerTube_);
+            cookController_.UI = userInterface_;
+
+            userInterface_ = new UserInterface();//mangler sine parametre 
+        }
+
+        [Test]
+        public void ddd()
+        {
+            //ffff
+        }
+    }
+}
