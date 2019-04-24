0162e49080d3423597a825c3108b0d5abd70664d
MathiasAKAchampen <201509795@post.au.dk>
2019-04-24 15:57:17 +0200

    Worked in IT4 and IT4b

--- Microwave.test.integration/IT4_UICookController.cs
+++ Microwave.test.integration/IT4_UICookController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace Microwave.test.integration
 {
@@ -16,12 +18,13 @@ namespace Microwave.test.integration
         private ILight light_;
         private IDisplay display_;
         private IOutput output_;
-        private ITimer timer_;
         private IButton powerButton_;
         private IButton timeButton_;
         private IButton startCancelButton_;
         private IDoor door_;
+        private ITimer fakeTimer_;
 
+        private Timer timer_;
         private PowerTube powerTube_;
         private CookController UUTcookController_;
         private UserInterface UUTUserInterface_;
@@ -31,7 +34,6 @@ namespace Microwave.test.integration
         [SetUp]
         public void setUp()
         {
-            timer_ = Substitute.For<ITimer>();
             output_ = Substitute.For<IOutput>();
             light_ = Substitute.For<ILight>();
             display_ = Substitute.For<IDisplay>();
@@ -39,8 +41,8 @@ namespace Microwave.test.integration
             timeButton_ = Substitute.For<IButton>();
             startCancelButton_ = Substitute.For<IButton>();
             door_ = Substitute.For<IDoor>();
-
-
+            
+            timer_ = new Timer();
             powerTube_ = new PowerTube(output_);
             UUTcookController_ = new CookController(timer_, display_, powerTube_);
             UUTUserInterface_ = new UserInterface(powerButton_, timeButton_, startCancelButton_, door_, display_, light_, UUTcookController_);
@@ -50,7 +52,7 @@ namespace Microwave.test.integration
         }
 
         [Test]
-        public void powerIsSet_PowerTimeAndStartButtonPressed_()
+        public void powerIsSet_PowerTimeAndStartButtonPressed_outputRecievedString()
         {
             powerButton_.Pressed += Raise.Event();
             powerButton_.Pressed += Raise.Event();
@@ -61,20 +63,80 @@ namespace Microwave.test.integration
             
         }
 
-        [Test]
-        public void testmemore()
+        [TestCase(1,50)]
+        [TestCase(5, 250)]
+        [TestCase(10, 500)]
+        [TestCase(14, 700)]
+        [TestCase(15, 50)]
+        public void powerIsSet_ClicksOnPowerButton_outputRecievesStringContainsCorrectWatt(int s1, int expectedWatt)
         {
-            powerButton_.Pressed += Raise.Event();
-            powerButton_.Pressed += Raise.Event();
+            for (int i = 0; i < s1; i++)
+            {
+                powerButton_.Pressed += Raise.Event();
+            }
+            
             timeButton_.Pressed += Raise.Event();
             startCancelButton_.Pressed += Raise.Event();
 
-            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains(Convert.ToString(100))));
+            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains(Convert.ToString(expectedWatt))));
 
         }
 
+        [TestCase(2, 1100, 1, 59)]
+        [TestCase(2, 5400, 1, 55)]
+        [TestCase(12, 1100, 11, 59)]
+        [TestCase(4, 4200, 3, 56)]
+        [TestCase(1, 1100, 0, 59)]
+        public void timeIsSet_DifferentMinutesIn_displayRecivescorrectMinutesAndSeconds(int s1, int wait, int expectedMinutes, int expectedSeconds)
+        {
+            powerButton_.Pressed += Raise.Event();
 
-        //output_.Received().OutputLine(Arg.Is<string>(s => s.Contains("100")));
+            for (int i = 0; i < s1; i++)
+            {
+                timeButton_.Pressed += Raise.Event();
+            }
+            
+            startCancelButton_.Pressed += Raise.Event();
+
+            ManualResetEvent pause = new ManualResetEvent(false);
+            pause.WaitOne(wait);
+
+            display_.Received().ShowTime(expectedMinutes, expectedSeconds);
+        }
+
+        [Test]
+        public void turnOffMicroOven_TurnOnMicroovenAndTurnOff_outputRecievesOff()
+        {
+            powerButton_.Pressed += Raise.Event();
+            timeButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+
+            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains("off")));
+
+        }
+
+        [Test]
+        public void cookingIsDone_cookingIsDone_displayClear()
+        {
+            powerButton_.Pressed += Raise.Event();
+            timeButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+            UUTUserInterface_.CookingIsDone();
+
+            display_.Received().Clear();
+        }
+
+        [Test]
+        public void cookingIsDone_cookingIsDone_LightTurnOff()
+        {
+            powerButton_.Pressed += Raise.Event();
+            timeButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+            UUTUserInterface_.CookingIsDone();
+
+            light_.Received().TurnOff();
+        }
 
     }
 }
---
+++ Microwave.test.integration/IT4b_UICookController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.test.integration
+{
+    class IT4b_UICookController
+    {
+        // Denne klasse er oprettet for at udføre nogle test med en fake timer, for at kunne teste
+        // om metoden "stop" kaldes i timeren, når mikroovnen standses
+
+        private ILight light_;
+        private IDisplay display_;
+        private IOutput output_;
+        private IButton powerButton_;
+        private IButton timeButton_;
+        private IButton startCancelButton_;
+        private IDoor door_;
+        private ITimer timer_;
+        
+        private PowerTube powerTube_;
+        private CookController UUTcookController_;
+        private UserInterface UUTUserInterface_;
+
+
+
+        [SetUp]
+        public void setUp()
+        {
+            output_ = Substitute.For<IOutput>();
+            light_ = Substitute.For<ILight>();
+            display_ = Substitute.For<IDisplay>();
+            powerButton_ = Substitute.For<IButton>();
+            timeButton_ = Substitute.For<IButton>();
+            startCancelButton_ = Substitute.For<IButton>();
+            door_ = Substitute.For<IDoor>();
+            timer_ = Substitute.For<ITimer>();
+            
+            powerTube_ = new PowerTube(output_);
+            UUTcookController_ = new CookController(timer_, display_, powerTube_);
+            UUTUserInterface_ = new UserInterface(powerButton_, timeButton_, startCancelButton_, door_, display_, light_, UUTcookController_);
+
+            UUTcookController_.UI = UUTUserInterface_;
+
+        }
+
+        [Test]
+        public void turnOffMicroOven_TurnOnMicroovenAndTurnOff_timerRecievesStop()
+        {
+
+            powerButton_.Pressed += Raise.Event();
+            timeButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+
+            timer_.Received().Stop();
+
+        }
+
+        
+    }
+}
