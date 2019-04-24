3380fed7b3378e320cda09d0e459ddae4e5b90b0
MathiasAKAchampen <201509795@post.au.dk>
2019-04-24 17:41:27 +0200

    Created tests in IT7

--- Microwave.test.integration/IT7_AllButtonsUI.cs
+++ Microwave.test.integration/IT7_AllButtonsUI.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NUnit.Framework;
-using NSubstitute; 
+using NSubstitute;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace Microwave.test.integration
 {
@@ -21,7 +23,7 @@ namespace Microwave.test.integration
         private ILight light_; 
         private IDisplay display_;
         private IPowerTube powertube_;
-        private ITimer timer_;
+        private Timer timer_;
         private ICookController cookController_;
         private UserInterface uutUserInterface_;
         private IDoor door_; 
@@ -33,7 +35,7 @@ namespace Microwave.test.integration
             light_ = new Light(output_);
             display_ = new Display(output_);
             powertube_ = new PowerTube(output_);
-            timer_ = Substitute.For<ITimer>();
+            timer_ = new Timer();
             cookController_ = new CookController(timer_, display_, powertube_, uutUserInterface_);
             door_ = Substitute.For<IDoor>();
 
@@ -71,6 +73,59 @@ namespace Microwave.test.integration
             door_.Opened += Raise.Event();
             door_.Closed += Raise.Event();
             output_.Received(1).OutputLine(Arg.Is<string>(s => s.Contains("off")));
+        }
+
+        [TestCase(1, 50)]
+        [TestCase(5, 250)]
+        [TestCase(10, 500)]
+        [TestCase(14, 700)]
+        [TestCase(15, 50)]
+        public void powerButtonIsPressed_PowerSet_outputRecievesStringContainsCorrectWatt(int s1, int expectedWatt)
+        {
+            for (int i = 0; i < s1; i++)
+            {
+                uutPowerButton_.Press();
+            }
+
+            uutTimeButton_.Press();
+            uutStartCancelButton_.Press();
+
+            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains(Convert.ToString(expectedWatt))));
+
+        }
+
+        [TestCase(2, 1100, 1, 59)]
+        [TestCase(2, 5400, 1, 55)]
+        [TestCase(12, 1100, 11, 59)]
+        [TestCase(4, 4200, 3, 56)]
+        [TestCase(1, 1100, 0, 59)]
+        public void timeButtonIsPressed_TimeSet_displayRecivescorrectMinutesAndSeconds(int s1, int wait, int expectedMinutes, int expectedSeconds)
+        {
+            uutPowerButton_.Press();
+
+            for (int i = 0; i < s1; i++)
+            {
+                uutTimeButton_.Press();
+            }
+
+            uutStartCancelButton_.Press();
+            
+            ManualResetEvent pause = new ManualResetEvent(false);
+            pause.WaitOne(wait);
+
+            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains(Convert.ToString(expectedMinutes)+":"+Convert.ToString(expectedSeconds))));
+        }
+
+        [Test]
+        public void turnOffMicroOven_turnOnButtonAndTurnOfButton_outputRecievesOff()
+        {
+            uutPowerButton_.Press();
+            uutTimeButton_.Press();
+            uutStartCancelButton_.Press();
+            uutStartCancelButton_.Press();
+           
+            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains("off")));
+
         }
     }
 }
