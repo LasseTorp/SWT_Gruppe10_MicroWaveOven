c859a5747c9e269524235b28fb107656bc74865d
MathiasAKAchampen <201509795@post.au.dk>
2019-04-24 14:33:36 +0200

    Worked on IT4

--- Microwave.test.integration/IT4_UICookController.cs
+++ Microwave.test.integration/IT4_UICookController.cs
@@ -15,26 +15,66 @@ namespace Microwave.test.integration
     {
         private ILight light_;
         private IDisplay display_;
-        private IPowerTube powerTube_;
-        private ITimer timer_;
-        private ICookController cookController_;
-        private UserInterface UUTUserInterface_;
         private IOutput output_;
+        private ITimer timer_;
+        private IButton powerButton_;
+        private IButton timeButton_;
+        private IButton startCancelButton_;
+        private IDoor door_;
+
+        private PowerTube powerTube_;
+        private CookController UUTcookController_;
+        private UserInterface UUTUserInterface_;
+
+        
 
         [SetUp]
         public void setUp()
         {
+            timer_ = Substitute.For<ITimer>();
             output_ = Substitute.For<IOutput>();
             light_ = Substitute.For<ILight>();
             display_ = Substitute.For<IDisplay>();
-            timer_ = Substitute.For<ITimer>();
+            powerButton_ = Substitute.For<IButton>();
+            timeButton_ = Substitute.For<IButton>();
+            startCancelButton_ = Substitute.For<IButton>();
+            door_ = Substitute.For<IDoor>();
+
+
             powerTube_ = new PowerTube(output_);
+            UUTcookController_ = new CookController(timer_, display_, powerTube_);
+            UUTUserInterface_ = new UserInterface(powerButton_, timeButton_, startCancelButton_, door_, display_, light_, UUTcookController_);
 
-            cookController_ = new CookController(timer_, display_, powerTube_);
-
-            //UUTUserInterface_ = new UserInterface();
+            UUTcookController_.UI = UUTUserInterface_;
 
         }
+
+        [Test]
+        public void powerIsSet_PowerTimeAndStartButtonPressed_()
+        {
+            powerButton_.Pressed += Raise.Event();
+            powerButton_.Pressed += Raise.Event();
+            timeButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+
+            output_.Received().OutputLine(Arg.Any<string>());
+            
+        }
+
+        [Test]
+        public void testmemore()
+        {
+            powerButton_.Pressed += Raise.Event();
+            powerButton_.Pressed += Raise.Event();
+            timeButton_.Pressed += Raise.Event();
+            startCancelButton_.Pressed += Raise.Event();
+
+            output_.Received().OutputLine(Arg.Is<string>(s => s.Contains(Convert.ToString(100))));
+
+        }
+
+
+        //output_.Received().OutputLine(Arg.Is<string>(s => s.Contains("100")));
 
     }
 }
